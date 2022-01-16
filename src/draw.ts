b905edd0f8df8ae6b87a47a3824cd2e36e06c8e7
Sandy S. Kuo <kuo.sandys@gmail.com>
2022-01-16 19:27:57 +0100

    Check if context is null before executing drawing actions

--- src/draw.ts
+++ src/draw.ts
@@ -1,5 +1,10 @@
 export default function draw(canvas: HTMLCanvasElement) {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const ctx = canvas.getContext('2d');
+
+  if (!ctx) {
+    console.log('Unable to get context');
+    return;
+  }
+
   // Go wild.
 }
