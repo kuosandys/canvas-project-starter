e541a28c913fa58437fa2c9dc0cdf381919c2292
Sandy S. Kuo <kuo.sandys@gmail.com>
2022-01-19 20:55:08 +0100

    Set canvas to be a square; set min length, margin, border

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,13 @@ import draw from './draw';
 
 function initCanvas(): HTMLCanvasElement {
   const canvas = document.getElementById('main-canvas') as HTMLCanvasElement;
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+  const minLength = 300;
+  const padding = 10;
+  const length =
+    Math.max(Math.min(window.innerWidth, window.innerHeight), minLength) -
+    2 * padding;
+  canvas.width = length;
+  canvas.height = length;
   return canvas;
 }
 
