757511cf880d7474d5abb53e18d8b1b0fbfffccf
Sandy S. Kuo <kuo.sandys@gmail.com>
2022-01-30 23:06:06 +0100

    Add a button to regenerate

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import draw from './draw';
 function initCanvas(): HTMLCanvasElement {
   const canvas = document.getElementById('main-canvas') as HTMLCanvasElement;
   const minLength = 300;
-  const padding = 10;
+  const padding = 50;
   const length =
     Math.max(Math.min(window.innerWidth, window.innerHeight), minLength) -
     2 * padding;
@@ -14,7 +14,12 @@ function initCanvas(): HTMLCanvasElement {
 }
 
 function main() {
-  draw(initCanvas());
+  const canvas = initCanvas();
+
+  const regenerateButton = document.getElementById('regenerate');
+  regenerateButton?.addEventListener('click', () => draw(canvas));
+
+  draw(canvas);
 }
 
 main();
